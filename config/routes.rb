40d65e6321f0d93a7e93ef98b445b4453780dcd0
Marco Ricci <mr.ricci79@gmail.com>
2022-05-27 11:19:33 +0100

    styled buttons

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy
 end
